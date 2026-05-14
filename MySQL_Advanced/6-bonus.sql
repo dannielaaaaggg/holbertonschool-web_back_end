3d46020d531631f492c35482fc0eca0a967e4626
dannielaaaaggg <danielagomez.mendez09@gmail.com>
2026-05-14 00:54:26 -0600

    Create 6-bonus.sql

---
+++ MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,13 @@
+-- Script that creates a stored procedure AddBonus
+DELIMITER |
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    DECLARE project_id INT;
+    IF NOT EXISTS (SELECT id FROM projects WHERE name = project_name) THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END;
+|
+DELIMITER ;
